27f9266a786340f4816c3f61414bf1cd25840afb
Huy Pham <105614012+Huy-P@users.noreply.github.com>
2022-10-18 15:04:50 +1030

    Finished practical

--- Wk11prac/UserLogin.py
+++ Wk11prac/UserLogin.py
@@ -24,4 +24,57 @@ class AuthException(Exception):
 class UsernameAlreadyExists(AuthException):
     pass
 class PasswordTooShort(AuthException):
-    pass 
+    pass
+
+class InvalidUsername(AuthException):
+    pass
+
+class InvalidPassword(AuthException):
+    pass
+
+class Authenticator:
+    def __init__(self) -> None:
+        self.users = {}
+    def add_user(self, username, password):
+        if len(password) <= 6:
+            raise PasswordTooShort(password)
+        elif username in self.users:
+            raise UsernameAlreadyExists(username)
+        else:
+            self.users[username]=User(username,password)
+    
+    def login(self, username, password):
+        if username not in self.users:
+            raise InvalidUsername(username)
+        if not self.users[username].check_password(password):
+            raise InvalidPassword(password)
+    
+        self.users[username].is_logged_in = True
+        return True 
+    
+    def is_logged_in(self, username):
+        if username in self.users:
+            return self.users[username].is_logged_in
+        return False
+
+auth = Authenticator()
+try:
+    auth.add_user("johnny", "johnnypassword")
+    print(auth.is_logged_in("johnny"))
+    auth.login("johnny", "joepassword")
+except AuthException as e:
+    print(type(e), e.username)
+try:
+    auth.login("johnny", "johnnypassword")
+    print(auth.is_logged_in("johnny"))
+    auth.add_user("johnny", "newpassword")  
+except AuthException as e:
+    print(type(e).__name__, e.username)
+try:
+    auth.login("susi", "123")
+except AuthException as e:  
+    print(type(e).__name__, e.username)
+try:
+    auth.add_user("susi", "123")
+except AuthException as e:
+    print(type(e).__name__, e.username)
